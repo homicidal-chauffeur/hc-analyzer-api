6a040972e27bdf6fb224f0da0217d7c5a6d20605
Peter Bohm <peter@nextlogic.biz>
2019-08-10 15:17:30 +1000

    Added updating of tags

--- app/simulations/controllers/SimulationsController.scala
+++ app/simulations/controllers/SimulationsController.scala
@@ -5,9 +5,10 @@ import java.util.UUID
 import javax.inject.Inject
 import play.api.libs.json.{JsError, Json}
 import play.api.mvc.{AbstractController, ControllerComponents}
-import simulations.dao.{SimulationDAO, SimulationFilter}
+import simulations.dao.{SimTags, SimulationDAO, SimulationFilter}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 class SimulationsController @Inject()(cc: ControllerComponents, dao: SimulationDAO)
                                      (implicit exec: ExecutionContext) extends AbstractController(cc) {
@@ -25,6 +26,21 @@ class SimulationsController @Inject()(cc: ControllerComponents, dao: SimulationD
   def edit(id: Long) = Action.async {
     implicit request =>
       dao.item(id).map(json => Ok(json).as(JSON) )
+  }
+
+  def updateTags(id: Long) = Action.async(parse.json) {
+    implicit request =>
+      val simTags = request.body.validate[SimTags]
+
+      simTags.fold(
+        errors => Future(BadRequest(Json.obj("status" -> "KO", "msg" -> "Validation errors", "message" -> JsError.toJson(errors)))),
+        tags => {
+          dao.updateTags(tags).map {
+            case Success(res) => Ok(Json.obj("msg" -> "Tags updated successfully"))
+            case Failure(error) => UnprocessableEntity(Json.obj("status" -> "KO", "msg" -> error.getMessage))
+          }
+        }
+      )
   }
 
 }
